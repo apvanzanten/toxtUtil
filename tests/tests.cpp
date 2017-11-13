f842d4133f871762017d99b8eb9365393f6ee503
apvanzanten <apvanzanten@gmail.com>
2017-11-13 12:09:39 +0100

    Removed old and crappy splitOn methods (now only the iterator-based one is left.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,42 +1,6 @@
 #include <catch.hpp>
 
 #include "util.hpp"
-TEST_CASE("util::splitOn()", "[util]") {
-
-  SECTION("string single element delimiter, no occurence") {
-    const std::string in = "1234";
-    const std::string delim = "x";
-
-    const auto res = util::splitOn(in, delim);
-    REQUIRE(res == std::vector<std::string>({"1234"}));
-  }
-
-  SECTION("string: single character delimiter, multiple parts") {
-    const std::string in = "0x123x4567x8x9";
-    const char delimiter = 'x';
-
-    const auto res = util::splitOn(in, delimiter);
-
-    REQUIRE(res == std::vector<std::string>({"0", "123", "4567", "8", "9"}));
-  }
-
-  SECTION("string: multiple character delimiter, multiple parts") {
-    const std::string in = "0split123split4567split8split9";
-    const std::string delimiter = "split";
-
-    const auto res = util::splitOn(in, delimiter);
-
-    REQUIRE(res == std::vector<std::string>({"0", "123", "4567", "8", "9"}));
-  }
-  SECTION("vector<int>: multiple element delimiter, multiple parts") {
-    const std::vector<int> in = {0, 9, 9, 9, 1, 2, 3, 9, 9, 9, 4, 5, 6, 7, 9, 9, 9, 8, 9, 9, 9, 9};
-    const std::vector<int> delimiter = {9, 9, 9};
-
-    const auto res = util::splitOn(in, delimiter);
-
-    REQUIRE(res == std::vector<std::vector<int>>({{0}, {1, 2, 3}, {4, 5, 6, 7}, {8}, {9}}));
-  }
-}
 
 TEST_CASE("util::splitOn() iterator-based version", "[util]") {
   SECTION("string single element delimiter, no occurence") {
