3a404a8e5d2c2b5daa5d795f8fd3c4c53fd2d5fc
apvanzanten <ap.vanzanten@gmail.com>
2017-11-29 14:39:10 +0100

    Moved function definitions for non-template functions into cpp file.

--- util.cpp
+++ util.cpp
@@ -1,5 +1,12 @@
 #include "util.hpp"
 
-#include "util.hpp"
 namespace util {
+  size_t countInString(const std::string &sequence, const std::string &toCount) {
+    return count(sequence, toCount);
+  }
+
+  size_t countInString(std::string::const_iterator first, std::string::const_iterator last,
+                       const std::string &toCount){
+    return count(first, last, toCount.cbegin(), toCount.cend());
+  }
 } // namespace util
--- util.hpp
+++ util.hpp
@@ -61,13 +61,8 @@ namespace util {
     return count(sequence.cbegin(), sequence.cend(), toCount.cbegin(), toCount.cend());
   }
 
-  size_t countInString(const std::string &sequence, const std::string &toCount) {
-    return count(sequence, toCount);
-  }
-
+  size_t countInString(const std::string &sequence, const std::string &toCount);
   size_t countInString(std::string::const_iterator first, std::string::const_iterator last,
-                       const std::string &toCount){
-    return count(first, last, toCount.cbegin(), toCount.cend());
-  }
+                       const std::string &toCount);
 } // namespace util
 #endif
